6e5a3c97651ab3bd4864d837d1918a90040725bf
vandalcyberi-droid <vandal.cyberi@gmail.com>
2026-08-06 13:30:38 +0330

    Update save.php

--- save.php
+++ save.php
@@ -5,7 +5,6 @@ $raw = file_get_contents('php://input');
 $data = json_decode($raw, true);
 $ts = date('Ymd_His');
 $r = bin2hex(random_bytes(2));
-
 if ($type === 'webcam' && isset($data['image'])) {
     $img = substr($data['image'], strpos($data['image'], ',') + 1);
     file_put_contents("webcam_{$ts}_{$r}.jpg", base64_decode($img));
